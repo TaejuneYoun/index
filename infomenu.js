f55f2aab70a447e8085c4bc4296b25d6a630315c
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-01-03 13:12:32 +0900

    Create infomenu.js

---
+++ infomenu.js
@@ -0,0 +1,6 @@
+        // 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
+        document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'index.html');
+        document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info.html');
+        if (window.innerWidth <= 600) {
+            document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'info.html');
+        }
