535327df897d32046afa9b393a4518a1c06f77be
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-01-03 14:51:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,38 @@
 
 /*------------------------------------------------------------------------------------------------------------------*/
 // Slider
-      document.addEventListener("DOMContentLoaded", function () {
-          var imageNames = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+document.addEventListener("DOMContentLoaded", function () {
+    var isMobile = window.innerWidth <= 768; // Assume width 768px or below as mobile
 
-          function getRandomImageName() {
-              var randomIndex = Math.floor(Math.random() * imageNames.length);
-              return imageNames[randomIndex];
-          }
+    var desktopImageNames = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+    var mobileImageNames = ["m1.png", "m2.png", "m3.png", "m4.png", "m5.png", "m6.png"];
 
-          function updateImages() {
-              var slides = document.querySelector(".slides");
-              var images = document.querySelectorAll(".slide");
+    function getRandomImageName() {
+        var imageNames = isMobile ? mobileImageNames : desktopImageNames;
+        var randomIndex = Math.floor(Math.random() * imageNames.length);
+        return imageNames[randomIndex];
+    }
 
-              images.forEach(function (image, index) {
-                  var imageName = getRandomImageName();
-                  var imageUrl = "https://raw.githubusercontent.com/TaejuneYoun/index/main/assets/" + imageName;
-                  image.src = imageUrl;
-                  image.alt = "Slide " + (index + 1);
-              });
+    function updateImages() {
+        var slides = document.querySelector(".slides");
+        var images = document.querySelectorAll(".slide");
 
-              slides.style.transform = "translateX(0)";
-          }
+        images.forEach(function (image, index) {
+            var imageName = getRandomImageName();
+            var imageUrl = "https://raw.githubusercontent.com/TaejuneYoun/index/main/assets/" + imageName;
+            image.src = imageUrl;
+            image.alt = "Slide " + (index + 1);
+        });
 
-          updateImages();
-      });
+        slides.style.transform = "translateX(0)";
+    }
+
+    updateImages();
+
+    // Optional: Add an event listener to handle window resize
+    window.addEventListener("resize", function () {
+        isMobile = window.innerWidth <= 576;
+        updateImages();
+    });
+});
 
