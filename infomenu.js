1fbd3757aef03211a4f90bc8cc8754be9dda6121
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-01-04 10:36:02 +0900

    Delete infomenu.js

--- infomenu.js
+++
@@ -1,6 +0,0 @@
-// 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
-document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'index.html');
-document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info.html');
-if (window.innerWidth <= 576) {
-        document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'info.html');
-        }
