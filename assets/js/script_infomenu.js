ee0dc942097cbf789b846ee3a237fde6a84e4a9d
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-01-08 03:49:50 +0900

    Delete assets directory

--- assets/js/script_infomenu.js
+++
@@ -1,6 +0,0 @@
-// 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
-document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'index.htm');
-document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info.htm');
-if (window.innerWidth <= 576) {
-        document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'info.htm');
-        }
