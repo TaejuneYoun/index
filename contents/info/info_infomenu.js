1df784a1fd5ece4083158e4149bb4b53c89382f7
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-03-12 18:45:23 +0900

    Update info_infomenu.js

--- contents/info/info_infomenu.js
+++ contents/info/info_infomenu.js
@@ -1,6 +1,6 @@
 // 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
 document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'index.html');
-document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info.html');
+document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info_pc.html');
 if (window.innerWidth <= 576) {
         document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'info.html');
         }
