f3a5533433bc32cd3c8bb19463bd8666327a42df
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-03-12 18:40:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------------------------------------------*/
 // header
         document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'index.html');
-        document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info_pc.html');
+        document.getElementById('infoLink').addEventListener('click', () => window.location.href = 'info_pc.htm');
 
         // 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
         if (window.innerWidth <= 576) {
