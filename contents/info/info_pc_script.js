ed1db9c04ca0b6405208c28bba0211245b34e285
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-12-26 05:59:15 +0900

    Update info_pc_script.js

--- contents/info/info_pc_script.js
+++ contents/info/info_pc_script.js
@@ -2,7 +2,7 @@ var XR;
 
 function initXR() {
   var nameOfDiv = "info_pc";
-  var folderUrl = "https://raw.githubusercontent.com/TaejuneYoun/index/refs/heads/main/contents/info/animations/";
+  var folderUrl = "../contents/info/animations/";
   var viewPortWidth = 1920;
   var viewPortHeight = 1080;
   var backgroundColor = "#FFFFFF";
