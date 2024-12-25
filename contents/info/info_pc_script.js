34be9df1e29e2d2cb078f815d492710cb9f45418
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-12-26 06:35:45 +0900

    Update info_pc_script.js

--- contents/info/info_pc_script.js
+++ contents/info/info_pc_script.js
@@ -1,9 +1,12 @@
+var XR;
+
 function initXR() {
   var nameOfDiv = "info_pc";
+  var folderName = "animations";
   var viewPortWidth = 1920;
   var viewPortHeight = 1080;
   var backgroundColor = "#FFFFFF";
-  var uCount = 151; // 이미지의 수 (0_0.jpg ~ 0_150.jpg)
+  var uCount = 151;
   var vCount = 1;
   var uWrap = true;
   var vWrap = false;
@@ -25,47 +28,8 @@ function initXR() {
   var vReverse = false;
   var hotspots = {};
   var isIBooksWidget = false;
-
-  // 이미지 URL 생성 함수
-  function getImageUrl(index) {
-    const baseUrl = "https://raw.githubusercontent.com/TaejuneYoun/index/refs/heads/main/contents/info/animations/";
-    return `${baseUrl}0_${index}.${imageExtension}`;
-  }
-
-  // 이미지 URL 리스트 생성
-  var imageUrls = [];
-  for (var i = 0; i < uCount; i++) {
-    imageUrls.push(getImageUrl(i));
-  }
-
-  // XR 객체 생성 (이미지 URL 리스트를 사용)
-  XR = new XR(
-    nameOfDiv,
-    imageUrls, // 이미지 URL 리스트 전달
-    viewPortWidth,
-    viewPortHeight,
-    backgroundColor,
-    uCount,
-    vCount,
-    uWrap,
-    vWrap,
-    uMouseSensitivity,
-    vMouseSensitivity,
-    uStartIndex,
-    vStartIndex,
-    minZoom,
-    maxZoom,
-    rotationDamping,
-    downScaleToBrowser,
-    addDownScaleGUIButton,
-    downloadOnInteraction,
-    imageExtension,
-    showLoading,
-    loadingIcon,
-    allowFullscreen,
-    uReverse,
-    vReverse,
-    hotspots,
-    isIBooksWidget
-  );
+  
+  XR = new XR(nameOfDiv,folderName,viewPortWidth,viewPortHeight,backgroundColor,uCount,vCount,uWrap,vWrap,uMouseSensitivity,vMouseSensitivity,uStartIndex,vStartIndex,minZoom,maxZoom,rotationDamping,downScaleToBrowser,addDownScaleGUIButton,downloadOnInteraction,imageExtension,showLoading,loadingIcon,allowFullscreen,uReverse,vReverse,hotspots,isIBooksWidget);
 }
+
+window.onload = initXR;
