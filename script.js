2b4ac8037e516ce9934f5924587b13f98e3c4b9b
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2025-02-13 21:38:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,34 +9,6 @@
         }
 
 /*------------------------------------------------------------------------------------------------------------------*/
-// Slider
-document.addEventListener("DOMContentLoaded", function () {
-    var isMobile = window.innerWidth <= 768; // Assume width 768px or below as mobile
-
-    var desktopImageNames = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
-    var mobileImageNames = ["m1.png", "m2.png", "m3.png", "m4.png", "m5.png", "m6.png"];
-
-    function getRandomImageName() {
-        var imageNames = isMobile ? mobileImageNames : desktopImageNames;
-        var randomIndex = Math.floor(Math.random() * imageNames.length);
-        return imageNames[randomIndex];
-    }
-
-    function updateImages() {
-        var slides = document.querySelector(".slides");
-        var images = document.querySelectorAll(".slide");
-
-        images.forEach(function (image, index) {
-            var imageName = getRandomImageName();
-            var imageUrl = "https://raw.githubusercontent.com/TaejuneYoun/index/main/assets/" + imageName;
-            image.src = imageUrl;
-            image.alt = "Slide " + (index + 1);
-        });
-
-        slides.style.transform = "translateX(0)";
-    }
-
-    updateImages();
 
     // Optional: Add an event listener to handle window resize
     window.addEventListener("resize", function () {
