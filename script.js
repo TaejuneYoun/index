463f28c0b104cb28e69122e50c0341f9e017be69
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2025-02-15 15:46:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
         // 모바일 버전에서 Taejune Youn 클릭 시 info.html로 이동
         if (window.innerWidth <= 576) {
-            document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'info.html');
+            document.getElementById('mainLink').addEventListener('click', () => window.location.href = 'https://www.linkedin.com/in/taejuneyoun/');
         }
 /*------------------------------------------------------------------------------------------------------------------*/
 
